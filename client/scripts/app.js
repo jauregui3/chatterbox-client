59afde08c612791685a27efd07175dacc1b22df5
Juancarlos Jauregui <jauregui3@gmail.com>
2017-05-26 20:19:19 -0700

    app.fetch and app.send work!!!

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -2,12 +2,12 @@ var app = {};
 
 app.server = 'http://parse.hrr.hackreactor.com/chatterbox/classes/messages';
 
+
 app.messages = [];
 
 app.friends = [];
 
 app.rooms = [];
-
 
 app.init = function () {
   app.fetch();
@@ -15,50 +15,44 @@ app.init = function () {
 };
 
 app.send = function(message) {
-
-  $("form").submit(function(){
-    var text = $("form").getElementById('text');
-    message = {
-      username: newsearch,
-      text: text,
-      roomname: 'test'
-    };
-    console.log(message);
-    $.ajax({
-      // This is the url you should use to communicate with the parse API server.
-      url: app.server,
-      type: 'POST',
-      data: JSON.stringify(message),
-      contentType: 'application/json',
-      success: function (data) {
-        console.log('chatterbox: Message sent');
-      },
-      error: function (data) {
-        // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
-        console.error('chatterbox: Failed to send message', data);
-      }
-    });
+  $.ajax({
+    // This is the url you should use to communicate with the parse API server.
+    url: app.server,
+    type: 'POST',
+    data: JSON.stringify(message),
+    contentType: 'application/json',
+    success: function (data) {
+      console.log('chatterbox: Message sent');
+    },
+    error: function (data) {
+      // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+      console.error('chatterbox: Failed to send message', data);
+    }
   });
 };
 
 app.fetch = function() {
-
   $.ajax({
     // This is the url you should use to communicate with the parse API server.
     url: app.server,
     type: 'GET',
     // contentType: 'application/json',
     success: function(data) {
-      //console.log(message.results);
       data.results.forEach(function (message) {
-        app.append(message.text, message.username);
+        app.messages.push(message);
+        /*if (app.rooms.hasOwnProperty[message.roomname]){
+         app.rooms[message.roomname].push(message);
+        } else {
+         app.rooms[message.roomname] = [];
+         app.rooms[message.roomname].push(message);
+        }*/
       });
     },
   });
 };
 
 app.prepend = function(text, username) {
-  $('#chats').prepend(`<div>${username}: ${text}</div>`);
+  $('#chats').prepend(`<div class="messageWindow">${username}: ${text}</div>`);
 };
 
 $(document).ready(function() {
@@ -85,3 +79,12 @@ $(document).ready(function() {
 //Display all messages by friends sent in bold
 
 //Add ability to clear all chat messages
+
+/*  $("form").submit(function(){
+    var text = $("form").getElementById('text');
+    message = {
+      username: newsearch,
+      text: text,
+      roomname: 'test'
+    };
+    console.log(message);*/
